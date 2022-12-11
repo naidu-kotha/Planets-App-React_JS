8a5c3a7291caeedf11158bc6601af87f99827126
naidu-kotha <kpssnaidu@gmail.com>
2022-12-11 07:13:23 +0000

    planets app

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const PlanetItem = props => {
+  const {planetDetails} = props
+  const {name, description, imageUrl} = planetDetails
+
+  return (
+    <div className="planet-container">
+      <img src={imageUrl} alt={`planet ${name}`} className="planet-image" />
+      <h1 className="planet-name">{name}</h1>
+      <p className="desc">{description}</p>
+    </div>
+  )
+}
+
+export default PlanetItem
